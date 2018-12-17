92be95ce813b310f7e1d316bbaf14949d9ba13db
cheng <792702352@qq.com>
2018-12-17 17:51:27 +0800

    :tada: Initial commit.

---
+++ src/main/java/com/cheng/monitor/tuning/MonitorTuningApplication.java
@@ -0,0 +1,14 @@
+package com.cheng.monitor.tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorTuningApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/cheng/monitor/tuning/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.cheng.monitor.tuning;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MonitorTuningApplication.class);
+    }
+
+}
+
