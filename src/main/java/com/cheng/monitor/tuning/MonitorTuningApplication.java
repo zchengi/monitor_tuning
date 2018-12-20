7e12ff06a27d30fb49a3120badcc631791dba43e
cheng <792702352@qq.com>
2018-12-20 15:23:50 +0800

    :package: Updating compiled files .

--- src/main/java/com/cheng/monitor/tuning/MonitorTuningApplication.java
+++ src/main/java/com/cheng/monitor/tuning/MonitorTuningApplication.java
@@ -2,19 +2,12 @@ package com.cheng.monitor.tuning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MonitorTuningApplication extends SpringBootServletInitializer {
+public class MonitorTuningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorTuningApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MonitorTuningApplication.class);
     }
 }
 
